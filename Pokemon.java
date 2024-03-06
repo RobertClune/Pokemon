83c223e9dbcf0d3aba2d277efa4e610a26810456
RobertClune <123131085+RobertClune@users.noreply.github.com>
2024-03-05 19:25:06 -0500

    Create Pokemon.java

---
+++ Pokemon.java
@@ -0,0 +1,16 @@
+/**
+ * Useful constants for working with the Pokemon collection
+ *
+ */
+  public  class PokeFields {
+          public static final String NAME = "name";
+          public static final String ATTACK = "attack";
+          public static final String DEFENSE = "defense";
+          public static final String EVOLVELEVEL = "evolveLevel";
+          public static final String EVOLVETO = "evolveTo";
+          public static final String TYPE = "type";
+          public static  final String MOVES = "moves";
+          public static final String CURVE = "curve";
+          public static final String LEVELS = "levels";
+          public static final String PROB = "probability";
+    }
